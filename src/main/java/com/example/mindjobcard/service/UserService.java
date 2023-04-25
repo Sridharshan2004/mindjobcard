8cbf689ecb1ba6ac3702e5e732dc23e1c5691db6
nandhakumar2611 <unandhakumar2000@gmail.com>
2023-04-25 11:48:16 +0530

    first commit

---
+++ src/main/java/com/example/mindjobcard/service/UserService.java
@@ -0,0 +1,24 @@
+package com.example.mindjobcard.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.mindjobcard.model.User;
+
+public interface UserService {
+	
+	Optional<User> findByUsername(String username);
+	
+	Boolean existsByUsername(String username);
+	
+	Boolean existsByEmail(String email);
+		
+	User saveuser(User user);
+	
+	List<User> getAllUser();
+	
+	void deleteById(Long id);
+	
+	Optional<User> findById(Long id);
+
+}
