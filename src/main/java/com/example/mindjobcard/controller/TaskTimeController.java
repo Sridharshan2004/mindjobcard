1ee63003ac3f9c0101fababafa36e1671a9a26ce
nandhakumar2611 <unandhakumar2000@gmail.com>
2023-05-09 10:51:00 +0530

    first commit

--- src/main/java/com/example/mindjobcard/controller/TaskTimeController.java
+++ src/main/java/com/example/mindjobcard/controller/TaskTimeController.java
@@ -1,9 +1,13 @@
 package com.example.mindjobcard.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,6 +39,19 @@ public class TaskTimeController {
 		}).orElseThrow(() -> new ResourceNotFoundException("TaskList Not Found"));
 		
 		return new ResponseEntity<TaskTime>(taskTime,HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/tasktimes")
+	public ResponseEntity<List<TaskTime>> getAllTaskTime() {
+		
+		List<TaskTime> taskTimes = new ArrayList<TaskTime>();
+		
+		taskTimeService.getAllTaskTime().forEach(taskTimes::add);
+		
+		if(taskTimes.isEmpty()) {
+			return new ResponseEntity<List<TaskTime>>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<List<TaskTime>>(taskTimes,HttpStatus.OK);
 	}
 
 }
